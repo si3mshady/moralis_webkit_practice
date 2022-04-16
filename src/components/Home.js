8e4dc1dd8aafa9ad9be83cd6e26bc41bc484a9e0
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2022-04-16 18:02:43 -0500

    save point

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,40 @@
 import './Home.css'
 import React from 'react'
-import { ConnectButton, Icon} from 'web3uikit'
+import { Tab, ConnectButton, Icon, TabList} from 'web3uikit'
 import Logo from '../images/Netflix.svg'
 export default function Home() {
   return (
     <>
     <div className="navBar">
 
+    <div className='leftSideContainer'>
     <img src={Logo}/> 
-
-      <div className="connectionContainer">                
+    <div className='tabContainer'>
+                    
+                    <TabList tabStyle='bar'>
           
-        <Icon fill="white" size={24} svg="bell" />      
-        <ConnectButton />
-      </div>
-       
+                    <Tab tabKey={1} tabName={"Favorites"} > </Tab>
+                    <Tab tabKey={2} tabName={"Movies"} > </Tab>
+                    <Tab tabKey={3} tabName={"Documentaries"} > </Tab>
+          
+                    </TabList>         
+                   
+                  </div>
+                  
+      
+    </div>
+    
+        <div className="connectionContainer">                
+          
+          <Icon fill="white" size={24} svg="bell" />      
+          
+          
+          
+          <ConnectButton />
+        </div>
+
+      
+      
     </div>
 
   
